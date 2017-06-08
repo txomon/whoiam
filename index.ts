b5cec7a028d1aaa7b10c86857308bcdb61e20b19
Javier Domingo Cansino <javier@jinnapp.com>
2017-06-08 15:26:52 +0100

    Generating more bindings with promise fun!

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { IAM, AWSError } from "aws-sdk"
+import * as AWS from "aws-sdk"
 
 
 class Resource {
@@ -121,68 +121,82 @@ class Group {
     }
 }
 
-
 class AWSStore {
-    iam_cli: IAM;
-    policies: Map<string, IAM.Policy>;
-    users: Map<string, IAM.User>;
-    groups: Map<string, IAM.Group>;
+    iam_cli: AWS.IAM;
+    policies: Map<string, AWS.IAM.Policy>;
+    users: Map<string, AWS.IAM.User>;
+    groups: Map<string, AWS.IAM.Group>;
 
     constructor() {
-        this.iam_cli = new IAM()
+        this.iam_cli = new AWS.IAM()
         this.policies = new Map()
         this.users = new Map()
         this.groups = new Map()
     }
 
     fetchPolicies(marker?: string): Promise<Map<string, object>> {
-
-        var params: IAM.ListPoliciesRequest = { MaxItems: 1000 }
+        var params: AWS.IAM.ListPoliciesRequest = { MaxItems: 1000 }
         if (marker != null) {
             params.Marker = marker
         }
-
         return new Promise((resolve, reject) => {
-            var responsePromise = this.iam_cli.listPolicies(params).promise()
-            responsePromise.then((value: IAM.ListPoliciesResponse) => {
-                var policies = new Map<string, object>()
-                if (value.Policies) {
-                    for (let iamPolicy of value.Policies) {
-                        if (!iamPolicy.PolicyName) {
-                            console.log(`Policy ${iamPolicy} is not created`)
-                            continue
+            this.iam_cli
+                .listPolicies(params).promise()
+                .then((value: AWS.IAM.ListPoliciesResponse) => {
+                    var policies = new Map<string, object>()
+                    if (value.Policies) {
+                        for (let iamPolicy of value.Policies) {
+                            if (!iamPolicy.PolicyName) {
+                                console.log(`Policy ${iamPolicy} is not created`)
+                                continue
+                            }
+                            policies.set(iamPolicy.PolicyName, iamPolicy)
                         }
-                        policies.set(iamPolicy.PolicyName, iamPolicy)
                     }
-                }
-                if (value.IsTruncated) {
-                    this.fetchPolicies(value.Marker).then((subvalue) => {
-                        policies = new Map([...policies, ...subvalue])
+                    if (value.IsTruncated) {
+                        this.fetchPolicies(value.Marker).then((subvalue) => {
+                            policies = new Map([...policies, ...subvalue])
+                            resolve(policies)
+                        })
+                    } else {
                         resolve(policies)
-                    })
-                } else {
-                    resolve(policies)
-                }
-
-            })
+                    }
+                })
         })
     }
 
-    fetchUsers(): Promise<IAM.ListUsersResponse> {
-        var usersPromise = new Promise<IAM.ListUsersResponse>((resolve, reject) => {
-            this.iam_cli.listUsers((err: AWSError, data: IAM.ListUsersResponse) => {
-                if (err) {
-                    console.log(`AWS:IAM:ListUsers failed. ${err}`)
-                    reject(err)
-                } else {
-                    resolve(data)
-                }
-            })
+    fetchUsers(marker?: string): Promise<Map<string, object>> {
+        var params: AWS.IAM.ListUsersRequest = { MaxItems: 1000 }
+        if (marker != null) {
+            params.Marker = marker
+        }
+        return new Promise((resolve, reject) => {
+            this.iam_cli
+                .listUsers(params).promise()
+                .then((value: AWS.IAM.ListUsersResponse) => {
+                    var policies = new Map<string, object>()
+                    if (value.Users) {
+                        for (let iamUser of value.Users) {
+                            if (!iamUser.UserName) {
+                                console.log(`Policy ${iamUser} is not created`)
+                                continue
+                            }
+                            policies.set(iamUser.UserName, iamUser)
+                        }
+                    }
+                    if (value.IsTruncated) {
+                        this.fetchPolicies(value.Marker).then((subvalue) => {
+                            policies = new Map([...policies, ...subvalue])
+                            resolve(policies)
+                        })
+                    } else {
+                        resolve(policies)
+                    }
+                })
         })
-        return usersPromise
     }
 
-    fetchGroups(): Promise<IAM.ListGroupsResponse> {
+    fetchGroups(): Promise<AWS.IAM.ListGroupsResponse> {
 
     }
 
